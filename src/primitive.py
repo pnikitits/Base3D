7b2e94ef0ecbd3d65a2ac3f7bcab5d2ff25ecf1d
pnikitits <112090831+pnikitits@users.noreply.github.com>
2024-10-05 19:15:07 +0200

    [ADD] => skybox

--- src/primitive.py
+++ src/primitive.py
@@ -2,10 +2,10 @@ from .vec import vec3
 
 
 
-def sphere(self, radius: float):
+def sphere(app, radius: float):
     # Load a model for now, will implement a sphere primitive later
     path = "/Users/pierrenikitits/Documents/GitHub/Base3D/assets/models/sphere.obj"
-    sphere = self.loader.loadModel(path)
+    sphere = app.loader.loadModel(path)
     sphere.setScale(radius)
     return sphere
     
@@ -22,8 +22,10 @@ def cone(radius: float, height: float, pos: vec3):
 def torus(radius: float, thickness: float, pos: vec3):
     pass
 
-def plane(size: vec3, pos: vec3):
-    pass
+def plane(app):
+    path = "/Users/pierrenikitits/Documents/GitHub/Base3D/assets/models/plane.obj"
+    plane = app.loader.loadModel(path)
+    return plane
 
 def quad(size: vec3, pos: vec3):
     pass
--- src/skybox.py
+++ src/skybox.py
@@ -1,3 +1,49 @@
+from .primitive import plane
+import os
+from panda3d.core import TextureStage
+
+
+
 class Skybox:
-    def __init__(self, path: str, size: float):
-        pass
+    def __init__(self, app, path: str, size: float):
+        
+        names = [
+            'top',
+            'bottom',
+            'right',
+            'left',
+            'front',
+            'back'
+        ]
+        
+        positions = [
+            (0, 0, size),
+            (0, 0, -size),
+            (size, 0, 0),
+            (-size, 0, 0),
+            (0, size, 0),
+            (0, -size, 0)
+        ]
+        
+        rotations = [
+            (0, -90, 0),
+            (0, 90, 0),
+            (90, 0, 180),
+            (-90, 0, 180),
+            (-180, 0, 180),
+            (0, 0, 180)
+        ]
+        
+        self._planes = []
+        for i in range(1,7):
+            name = f"{names[i-1]}.png"
+            texture = app.loader.loadTexture(os.path.join(path, name))
+            plane_i = plane(app)
+            base_ts = TextureStage('base_ts')
+            base_ts.setMode(TextureStage.MReplace)
+            plane_i.setTexture(base_ts, texture)
+            plane_i.setScale(size)
+            plane_i.setPos(positions[i-1])
+            plane_i.setHpr(rotations[i-1])
+            plane_i.reparentTo(app.render)
+            self._planes.append(plane_i)
