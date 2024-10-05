dc5fa62762af4030234e9f5d98bde65835d3bd05
pnikitits <112090831+pnikitits@users.noreply.github.com>
2024-10-05 18:23:35 +0200

    [ADD] => basic camera and light

--- src/camera.py
+++ src/camera.py
@@ -3,5 +3,35 @@ from .vec import vec3
 
 
 class Camera:
-    def __init__(self, pos:vec3, rot:vec3):
-        pass
+    def __init__(self, app, pos: vec3=vec3(1, 1, 1), lookAt: vec3=vec3(0, 0, 0)):
+        
+        self._pos = pos
+        self._lookAt = lookAt
+        
+        app.disableMouse()
+        self._camera = app.camera
+        self._camera.setPos(pos.x, pos.y, pos.z)
+        self._camera.lookAt(lookAt.x, lookAt.y, lookAt.z)
+        
+        self._camLens = app.camLens
+        self._camLens.setNear(0.1)
+        self._camLens.setFar(100.0)
+        
+        
+    @property
+    def pos(self):
+        return self._pos
+    
+    @pos.setter
+    def pos(self, value):
+        self._pos = value
+        self._camera.setPos(value.x, value.y, value.z)
+        
+    @property
+    def lookAt(self):
+        return self._lookAt
+    
+    @lookAt.setter
+    def lookAt(self, value):
+        self._lookAt = value
+        self._camera.lookAt(value.x, value.y, value.z)
--- src/light.py
+++ src/light.py
@@ -1,7 +1,13 @@
-from .vec import vec3
+from .vec import vec3, vec4
+from panda3d.core import PointLight
 
 
 
 class Light:
-    def __init__(self, pos:vec3, color:vec3, intensity:float):
-        pass
+    def __init__(self, pos:vec3, color:vec4, render):
+        
+        plight = PointLight('plight')
+        plight.setColor(color.to_tuple())
+        light_np = render.attachNewNode(plight)
+        light_np.setPos(pos.x, pos.y, pos.z)
+        render.setLight(light_np)
